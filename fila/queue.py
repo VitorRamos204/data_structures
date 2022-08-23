34d4601a4fa1178b2cea0647638cd1abf49f50f8
Vitor Ramos de Senna <vitor.ramos2759@gmail.com>
2022-08-23 08:22:57 -0300

    Queue

--- fila/queue.py
+++ fila/queue.py
@@ -6,18 +6,20 @@ class Queue:
         self.first = None
         self.last = None
         self._size = 0
+
     def push(self, elem):
-        node = Node()
+        node = Node(elem)
         if self.last is None:
             self.last = node
         else:
             self.last.next = node
-            self.last = self.node
+            self.last = node
         if self.first is None:
             self.first = node
         self._size = self._size + 1
+
     def pop(self):
-        if self.first is None:
+        if self._size > 0:
             elem = self.first.data
             self.first = self.first.next
             self._size = self._size - 1
@@ -25,19 +27,26 @@ class Queue:
     raise IndexError('The queue is empty')
 
     def peek(self):
-        if self.first is None:
+        if self._size > 0:
             elem = self.first.data
             return elem
         raise IndexError('The queue is empty')
 
     def __len__(self):
-
+        return self._size
 
     def __repr__(self):
-
+        if self._size > 0:
+            r = ""
+            pointer = self.first
+            while pointer:
+                r = r + str(pointer.data) + " "
+                pointer = pointer.next
+            return r
+        return "Queue is empty"
 
     def __str__(self):
-
+        return self.__repr__()
 
 
 if __name__ == '__main__':
